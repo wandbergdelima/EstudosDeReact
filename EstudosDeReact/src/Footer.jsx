c621e8808acc7f84942c117b0532ee0ac1ae30ae
wandbergdelima <wandberg@gmail.com>
2022-03-30 23:26:06 -0300

    Meu primeiro código em react <3

---
+++ EstudosDeReact/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                footer aqui
+            </div>
+        );
+    }
+}
+
+export default Footer;
---
+++ EstudosDeReact/src/Header.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-3  col-md-3 col-sm-6 col-xs-6">
+                            <h3> Sobre nós </h3>
+                            <ul>
+                                <li> <a href="#"> Quem somos </a> </li>
+                                <li> <a href="#"> Política de privacidade </a> </li>
+                                <li> <a href="#"> Termos de uso </a> </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+export default Header;
